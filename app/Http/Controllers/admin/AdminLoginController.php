dfe3ace1f007166b3646f9094a02ead70ac6d44e
RinNguyen2002 <tuanngocnguyenhuu2002@gmail.com>
2024-03-17 12:07:21 +0700

    Category: CreateList

--- app/Http/Controllers/admin/AdminLoginController.php
+++ app/Http/Controllers/admin/AdminLoginController.php
@@ -9,28 +9,29 @@ use Illuminate\Support\Facades\Validator;
 
 class AdminLoginController extends Controller
 {
-    public function index() {
+    public function index()
+    {
         return view('admin.login');
     }
-    public function authenticate(Request $request) {
+    public function authenticate(Request $request)
+    {
 
-        $validator = Validator::make($request->all(),[
+        $validator = Validator::make($request->all(), [
             'email' => 'required|email',
             'password' => 'required'
         ]);
-        if($validator->passes()) {
-            if(Auth::guard('admin')->attempt(['email' => $request->email,'password'=> $request->password],$request->get('remember'))) {
+        if ($validator->passes()) {
+            if (Auth::guard('admin')->attempt(['email' => $request->email, 'password' => $request->password], $request->get('remember'))) {
 
                 $admin = Auth::guard('admin')->user();
                 if ($admin->role == 2) {
                     return redirect()->route('admin.dashboard');
                 } else {
                     Auth::guard('admin')->logout();
-                    return redirect()->route('admin.login')->with('error','You are not authorized to access admin panel.');
+                    return redirect()->route('admin.login')->with('error', 'You are not authorized to access admin panel.');
                 }
-
             } else {
-                return redirect()->route('admin.login')->with('error','Either Email/Password is incorrect');
+                return redirect()->route('admin.login')->with('error', 'Either Email/Password is incorrect');
             }
         } else {
             return redirect()->route('admin.login')
@@ -38,5 +39,4 @@ class AdminLoginController extends Controller
                 ->withInput($request->only('email'));
         }
     }
-
 }
